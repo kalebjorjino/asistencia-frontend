e51a0b204e97c0b091cdcc1e9309d4b9ef7be8d1
Garcia Vasquez Jorjino <jgarcia@hrcallao.gob.pe>
2025-05-22 09:54:27 -0500

    imagen2 correcion

--- vite.config.js
+++ vite.config.js
@@ -24,12 +24,12 @@ export default defineConfig({
         scope: '/',
         icons: [
           {
-            src: '/img/icons/image2.png',
+            src: '/public/image2.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/img/icons/image2.png',
+            src: '/public/image2.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
